7e9f3dac4fdf1940208273327720a91b3d4c5530
Thomas Brettin <brettint@gmail.com>
2022-08-18 12:57:59 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ infile=${a[$PMI_RANK]}
 
 # Train or something on infile
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python simple_transformer.py --infile $infile
-echo "running python simple_transformer.py --infile $infile on $(hostname) with MPI Rank $PMI_RANK"
+echo "running python train.py --infile $infile on $(hostname) with MPI Rank $PMI_RANK"
 
