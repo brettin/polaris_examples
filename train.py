07c535487b66316a34d28d1e690122754860b471
Thomas Brettin <brettint@gmail.com>
2022-08-18 16:54:09 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 
 psr = argparse.ArgumentParser(description="input csv file")
-psr.add_argument("--in_file", default="df")
+psr.add_argument("--infile", default="df")
 args = vars(psr.parse_args())
 
 print(args)
