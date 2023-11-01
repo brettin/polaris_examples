e19749f054aea681a0f8673c3a9668c2268bca81
brettin <brettin@anl.gov>
2023-11-01 23:35:11 +0000

    added Archit's solution

--- sdf_to_smile/sdf_smiles_convert_multi.py
+++ sdf_to_smile/sdf_smiles_convert_multi.py
@@ -4,18 +4,35 @@ from rdkit import Chem
 
 def converter(file_name,outname=None):
     f = gzip.open(file_name, "rb")
-    sppl = Chem.ForwardSDMolSupplier(f)
+    fsppl = Chem.ForwardSDMolSupplier(f, sanitize=False)
     if outname != None:
         out_file = gzip.open(outname, "wb")
 
-    for mol in sppl:
+    for mol in fsppl:
         if mol is not None:# some compounds cannot be loaded.
-            smi = Chem.MolToSmiles(mol)
+
+            # Archit solution to wrong valence error
+            mol.UpdatePropertyCache(strict=False)
+            ps = Chem.DetectChemistryProblems(mol)
+            if ps:
+                for p in ps:
+                    # print(f"Error is {p}")
+                    if p.GetType()=='AtomValenceException':
+                        at = mol.GetAtomWithIdx(p.GetAtomIdx())
+                        if at.GetAtomicNum()==7 and at.GetFormalCharge()==0 and at.GetExplicitValence()>=4:
+                            at.SetFormalCharge(1)
+
+            Chem.SanitizeMol(mol)
+            Chem.Kekulize(mol)
+            mol = Chem.RemoveHs(mol)
+            smi = Chem.MolToSmiles(mol, kekuleSmiles=True)
             name = mol.GetProp("_Name")
+            
             if outname is None:
                 print(f"{smi}\t{name}")
             else: 
                 out_file.write(f"{smi}\t{name}\n".encode())
+
 
     if outname != None:
         out_file.close()
