1c9be1c8a184e0f7b4e646e129710f4eed36474a
brettin <brettin@anl.gov>
2023-11-04 16:40:19 +0000

    implement /dev/shm

--- sdf_to_smile/sdf_smiles_convert_multi.py
+++ sdf_to_smile/sdf_smiles_convert_multi.py
@@ -1,6 +1,8 @@
 import sys
 import gzip
+import os
 from rdkit import Chem
+
 
 def converter(file_name,outname=None):
     f = gzip.open(file_name, "rb")
@@ -38,11 +40,25 @@ def converter(file_name,outname=None):
         out_file.close()
 
 if __name__ == "__main__":
+    infile = sys.argv[1]
+    outdir = '/dev/shm/' + sys.argv[1]
+    os.makedirs(outdir, exist_ok=True)
 
     # the input is a file w a list of sdf file base names
-    with open(sys.argv[1]) as f:
+    call_count = 0
+    with open(infile) as f:
         for line in f:
               line = line.rstrip()
-              print("calling converter on basename {}".format(line))
-              converter(line + '.sdf.tgz', outname = line + '.smi.gz')
-            
+              parts = os.path.split(line)
+
+              if os.path.exists(outdir + '/' + parts[0]) == False:
+                  print("creating {}/{}".format(outdir, parts[0]))
+                  os.makedirs(outdir + '/' + parts[0])
+
+              outname = outdir + '/' + parts[0] + "/" + parts[1] + '.smi.gz'
+
+              call_count = call_count + 1
+              converter(line + '.sdf.tgz', outname=outname)
+
+    print('CALL_COUNT = {}'.format(call_count))
+
